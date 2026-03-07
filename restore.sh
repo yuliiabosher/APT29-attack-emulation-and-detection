235c309248ca30261b60f5317986007fda8a4fc0
Yulia <yulia.bosher@gmail.com>
2026-03-07 20:00:41 +0000

    Added the fact source file for APT29

--- restore.sh
+++ restore.sh
@@ -1,2 +1,2 @@
 cp -r /home/yulia/APT29-attack-emulation-and-detection/APT29.yaml /home/yulia/caldera5/plugins/emu/data/adversary-emulation-plans/apt29/Emulation_Plan/yaml/APT29.yaml
-
+cp -r /home/yulia/APT29-attack-emulation-and-detection/152ca62e-32b6-5c2a-84e8-89fedd44d621.yml /home/yulia/caldera5/plugins/emu/data/sources/152ca62e-32b6-5c2a-84e8-89fedd44d621.yml 
