7983d8cf48f6cb3e9b8edff32e3f05a70e276c58
Yuliia <yulia.bosher@gmail.com>
2026-06-17 20:23:14 +0100

    Delete backup.sh

--- backup.sh
+++
@@ -1,3 +0,0 @@
-cp -r /home/yulia/caldera5/plugins/emu/data/adversary-emulation-plans/apt29/Emulation_Plan/yaml/APT29.yaml /home/yulia/APT29-attack-emulation-and-detection
-cp -r /home/yulia/caldera5/plugins/emu/data/sources/152ca62e-32b6-5c2a-84e8-89fedd44d621.yml /home/yulia/APT29-attack-emulation-and-detection
-cp -r /home/yulia/caldera5/plugins/emu/payloads/upload.ps1 /home/yulia/APT29-attack-emulation-and-detection
